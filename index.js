187aefa5e91e0998936246854409bdd13cd3f7d9
Sergii Skorokhodov <sergii.skorokhodov@moonfare.com>
2020-11-10 13:43:59 +0100

    feat(*): initial commit
    make the cat say hello

---
+++ index.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk');
+
+const phrase = 'hi!';
+
+const catSay = `
+      ／＞　 フ
+     |  _　_ |
+   ／\` ミ＿xノ    ${phrase && `< ${phrase}`}
+ /　　 　　 |
+/　 ヽ　　 ﾉ
+`;
+
+console.log(chalk.white(catSay));
